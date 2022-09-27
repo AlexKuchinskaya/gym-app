d0541d582f021f4e8eb65dad37ce422618bca171
AlexKuchinskaya <alexkuchynsk@gmail.com>
2022-09-27 17:36:15 +0200

    finish heroBanner and start searchExercises

--- src/components/search-exercises/SearchExercises.js
+++ src/components/search-exercises/SearchExercises.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Box, Stack, Typography, Button, TextField } from '@mui/material';
 
 const SearchExercises = () => {
     return (
-        <div>
-            SearchExercises
-        </div>
+        <Stack alignItems="center" justifyContent="center" p="20px">
+            <Typography sx={{ fontSize: { lg: '44px', xs: '33px'} }} fontWeight={700} textAlign="center" mb="49px">Awesome Exercises You <br /> Should Know</Typography>
+        </Stack>
     )
 }
 
